5710e120503262fa216864947299ef9c40305809
Felipe-m-s <fs33143martins@gmail.com>
2025-10-01 10:01:53 -0300

    Finalizando atividade sobre arquivos

--- intro_arquivos/Atividade/main.cpp
+++ intro_arquivos/Atividade/main.cpp
@@ -6,47 +6,80 @@ using namespace std;
 
 int main() {
 
-    string nome;
-    float altura;
-    int idade;
+    int opcao;
     
-    ofstream arq;
-    arq.open("dados.in", ios::app);
-    if (!arq.is_open())
+    do
     {
-        cout << "Error ao abrir o arquivo!" << endl;
-        return 1;
-    }
-
-    cout << "Digite seu nome completo: " << endl;
-    getline(cin, nome);
-
-    cout << "Digite a sua altura: " << endl;
-    cin >> altura;
-    
-    cout << "Digite a sua idade: " << endl;
-    cin >> idade;
-
-    arq << "Nome: " << nome << endl;
-    arq << "Altura: " << altura << endl;
-    arq << "Idade: " << idade << endl;
-
-    ifstream arq;
-    arq.open("dados.in");
-    if (!arq.is_open())
-    {
-        cout << "Error ao abrir o arquivo!" << endl;
-        return 1;
-    }
-
-    char linha[255];
-
-    while (!arq.eof())
-    {
-       
+        cout << "1 - Cadastrar Pessoa" << endl;
+        cout << "2 - Listar Pessoas" << endl;
+        cout << "0 - Encerrar" << endl;
         
-    }
-    
+        cin >> opcao;
+        
+        switch (opcao)
+        {
+            case 1: {
+                string nome;
+                float altura;
+                int idade;
+
+                ofstream arq;
+                arq.open("dados.in", ios::app);
+                if (!arq.is_open())
+                {
+                    cout << "Error ao abrir o arquivo!" << endl;
+                    return 1;
+                }
+
+                cin.ignore();
+
+                cout << "Digite seu nome completo: " << endl;
+                getline(cin, nome);
+
+                cout << "Digite a sua altura: " << endl;
+                cin >> altura;
+
+                cout << "Digite a sua idade: " << endl;
+                cin >> idade;
+
+                arq << nome << endl;
+                arq << altura << endl;
+                arq << idade << endl;
+
+                arq.close();
+                break;
+            }
+        
+        case 2: {
+            string nome;
+            float altura;
+            int idade;
+            
+            ifstream arq;
+            arq.open("dados.in");
+            if (!arq.is_open())
+            {
+                cout << "Error ao abrir o arquivo!" << endl;
+                return 1;
+            }
+
+            while (getline(arq, nome) && (arq >> idade) && (arq >> altura))
+            {
+                cout << nome << endl;
+                cout << altura << " m" << endl;
+                cout << idade << " anos" << endl;
+                arq.ignore();
+            }
+            
+
+            arq.close();
+            break;
+        }
+        default:
+            break;
+        }
+
+    } while (opcao != 0);
 
     return 0;
 }
